1792fc63d233cf5de5c2990aacc2da295c1233aa
sang98455 <ramana55667@gmail.com>
2026-04-24 14:29:16 +0530

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "REPLACE_WITH_REAL_API_KEY",
-  authDomain: "REPLACE_WITH_REAL_AUTH_DOMAIN",
-  projectId: "REPLACE_WITH_REAL_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_REAL_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_REAL_SENDER_ID",
-  appId: "REPLACE_WITH_REAL_APP_ID",
+  apiKey: "AIzaSyCMD8AUDX11lycOKarl4s1B49_PQJJz2zY",
+  authDomain: "student-project-submission.firebaseapp.com",
+  projectId: "student-project-submission",
+  storageBucket: "student-project-submission.appspot.com",
+  messagingSenderId: "687401184985",
+  appId: "1:687401184985:web:c42b88d41d1af0d5e058c8",
+  measurementId: "G-S1P3R86LBH",
 };
 
 const app = initializeApp(firebaseConfig);
